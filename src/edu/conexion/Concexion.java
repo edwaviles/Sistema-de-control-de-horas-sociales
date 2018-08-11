e0e8ee5bd1c4e82f9c85ce124be89cb70430dedd
Ernesto Aviles <Ernesto Aviles@Ernest>
2018-08-11 14:05:26 -0600

    Proyecto vacio

---
+++ src/edu/conexion/Concexion.java
@@ -0,0 +1,41 @@
+
+package edu.conexion;
+
+import java.sql.*;
+/**
+ *
+ * @author Ernesto Aviles
+ */
+public class Concexion 
+{
+    Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+    public void conectar(){
+        try 
+        {
+            
+        }
+        catch (Exception e) 
+        {
+            
+        }
+    }
+    public  void desconectar()
+    {
+        try 
+        {
+            
+        }
+        catch (Exception e) 
+        {
+            
+        }
+    }
+}
